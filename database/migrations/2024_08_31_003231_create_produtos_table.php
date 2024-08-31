4dd21690a9e0e5c04bc91c85f62f0ed48b9b9686
FernandoAquinoPinheiro <fernando.pinheiro@etec.sp.gov.br>
2024-08-30 22:56:12 -0300

    first commit

---
+++ database/migrations/2024_08_31_003231_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id("codigo");
+            $table->string("tipoProduto");
+            $table->decimal("valorProduto",7,2);
+            $table->unsignedBigInteger("codigoClientefk");
+           
+            $table->timestamps();
+        });
+        Schema::table('produtos', function (Blueprint $table)
+        {
+            $table->foreign('codigoClientefk')->references('codigoCliente')->on('clientes')->onDelete('cascade');
+        });
+    }
+
+ 
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
