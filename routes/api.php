b17d1df268e9a46f17cdab79c8e413905831d407
FernandoAquinoPinheiro <fernando.pinheiro@etec.sp.gov.br>
2024-09-09 16:03:59 -0300

    rotas criadas

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,30 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
-
+use App\Http\Controllers\ProdutoController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/',function(){return response()->json(['Sucesso'=>true]);});
-Route::get('/Clientes',[ClienteController::class,'index']);
-Route::get('/Clientes/{id}',[ClienteController::class,'show']);
-Route::post('/Clientes',[ClienteController::class,'store']);
-Route::put('/Clientes/{id}',[ClienteController::class,'update']);
-Route::delete('/Clientes/{id}',[ClienteController::class,'destroy']);
+
+Route::get('/', function(){
+    return response()->json(["Sucesso" => true ]);
+});
+
+Route::prefix('clientes')->group(function(){
+
+Route::get('/',[ClienteController::class,'index']);
+Route::get('/{id}',[ClienteController::class,'show']);
+Route::post('/',[ClienteController::class,'store']);
+Route::put('/{id}',[ClienteController::class,'update']);
+Route::delete('{id}',[ClienteController::class,'destroy']);
+});
+
+
+Route::prefix('Produtos')->group(function(){
+
+    Route::get('/',[ProdutoController::class,'index']);
+    Route::get('/{id}',[ProdutoController::class,'show']);
+    Route::post('/',[ProdutoController::class,'store']);
+    Route::put('/{id}',[ProdutoController::class,'update']);
+    Route::delete('{id}',[ProdutoController::class,'destroy']);
+    });
